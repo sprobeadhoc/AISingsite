fd2257aa169d48b7a1a36b83b5423235778ceaf4
Brix Rodman B. Nessia <nessia.brb@AD.SPROBE.PH>
2018-10-29 18:04:15 +0800

    T13489 Layout Top Page

---
+++ wp-content/themes/AISing/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AISing</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img class ="logo" src="<?php echo get_template_directory_uri(); ?>/assets/sample.png">
+            </a>
+            <?php bootstrap_nav(); ?>
+            <div class="translation" >EN/JP</div>
+        </nav>
+    </header>
+<div class="container-fluid">
+    <img class ="img" src="<?php echo get_template_directory_uri(); ?>/assets/mech.jpg">
+</div>
+
+<div class="jumbotron">
+    <div class="col-sm-8 mx-auto">
+        <h1>This is our Business</h1>
+        <p>This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.
+        This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.
+        This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.This is a sample Business content.
+        </p>
+       
+    </div>
+</div>
+
+ 
+
